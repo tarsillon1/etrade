da08e66d25fcf772320a9980621b1022f28c36b9
ntarsillo <nicholastarsillo@Nicholass-MacBook-Pro.local>
2024-06-30 20:53:35 -0400

    fix

--- authorization.go
+++ authorization.go
@@ -1,26 +1,28 @@
 package etrade
 
 import (
+	"fmt"
+
 	"github.com/tarsillon1/oauth1"
 )
 
 const (
 	apiUrl          = "https://api.etrade.com"
-	authorizeUrl    = "https://us.etrade.com/e/t/etws/authorize"
+	authorizeUrl    = "https://us.etrade.com/e/t/etws/authorize?oauth_consumer_key=%s"
 	accessTokenUrl  = apiUrl + "/oauth/access_token"
 	requestTokenUrl = apiUrl + "/oauth/request_token"
 )
 
 // NewOAuth1Config creates an OAuth configuration with pre-configured endpoints for ETrade OAuth provider.
-func NewOAuth1Config(consumerKey, consumerSecret string) oauth1.Config {
+func NewOAuth1Config(consumerKey, consumerSecret, callbackUrl string) oauth1.Config {
 	return oauth1.Config{
 		ConsumerKey:    consumerKey,
 		ConsumerSecret: consumerSecret,
-		CallbackURL:    "oob",
+		CallbackURL:    callbackUrl,
 		Endpoint: oauth1.Endpoint{
 			AccessTokenURL:  accessTokenUrl,
 			RequestTokenURL: requestTokenUrl,
-			AuthorizeURL:    authorizeUrl,
+			AuthorizeURL:    fmt.Sprintf(authorizeUrl, consumerKey),
 		},
 		HTTPMethod: "GET",
 	}
